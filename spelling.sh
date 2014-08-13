4d2cbc323baf472e77d74907bcf52948e9410baa
I am not me <impeccabletiming@gmail.com>
2014-08-13 11:52:03 -0400

    Adding Tamagotchi, Hello Kitty, Hot Wheels, and Webkinz

--- spelling.sh
+++ spelling.sh
@@ -400,6 +400,27 @@ sed -i "s:wikianswers:Wikianswers:I" "$CORRECTED"
 
 sed -i "s:happend:happened:I" "$CORRECTED"
 
+#Spellcheck "Tamagotchi"
+
+sed -i "s:Tamagotchi:Tamagotchi:I" $CORRECTED
+sed -i "s:tomogotchi:Tamagotchi:I" $CORRECTED
+sed -i "s:Tamagatchi:Tamagotchi:I" $CORRECTED
+sed -i "s:tamogotchi:Tamagotchi:I" $CORRECTED
+
+#Spellcheck "Hello Kitty"
+
+sed -i "s:Hello Kitty:Hello Kitty:I" $CORRECTED
+sed -i "s:helloKitty:Hello Kitty:I" $CORRECTED
+
+#Spellcheck "Hot Wheels"
+
+sed -i "s:Hot Wheels:Hot Wheels:I" $CORRECTED
+sed -i "s: HotWheels : Hot Wheels :I" $CORRECTED
+
+#Spellcheck "Webkinz"
+
+sed -i "s: Webkinz: Webkinz:I" $CORRECTED
+
 #Final fixes and merge
 
 addbrackets "$NEWPAGES" "$CORRECTED"
