1f2dba512f84fec40c42377194a57a212cd50150
I am not me <impeccabletiming@gmail.com>
2014-08-15 23:59:11 -0400

    Adding fixes to Pokémon script

--- Entertainment/Franchises/Pokemon/Pokemon.sh
+++ Entertainment/Franchises/Pokemon/Pokemon.sh
@@ -19,6 +19,8 @@ sed -i "s:Pokémon White:Pokémon White:I" "$CORRECTED"
 sed -i "s:Pokémon Whte:Pokémon White:I" "$CORRECTED"
 sed -i "s:PokémonWhite:Pokémon White:I" "$CORRECTED"
 sed -i "s:Pokémon White2:Pokémon White 2:I" "$CORRECTED"
+sed -i "s:Pokémon Black and White 2:Pokémon Black 2 and Pokémon White 2:I" "$CORRECTED"
+sed -i "s:Pokémon Black and White:Pokémon Black and Pokémon White:I" "$CORRECTED"
 
 #Spellcheck "Pokémon Diamond"
 
@@ -38,6 +40,13 @@ sed -i "s:Pokémon HeartGold:Pokémon HeartGold:I" "$CORRECTED"
 sed -i "s:Pokémon Heart Gold:Pokémon HeartGold:I" "$CORRECTED"
 sed -i "s:PokémonHeartGold:Pokémon HeartGold:I" "$CORRECTED"
 sed -i "s:PokémonHeart Gold:Pokémon HeartGold:I" "$CORRECTED"
+
+#Spellcheck "Pokémon SoulSilver"
+
+sed -i "s:Pokémon SoulSilver:Pokémon SoulSilver:I" "$CORRECTED"
+sed -i "s:Pokémon soul silver:Pokémon SoulSilver:I" "$CORRECTED"
+sed -i "s:Pokémonsoulsilver:Pokémon SoulSilver:I" "$CORRECTED"
+sed -i "s:Pokémonsoul silver:Pokémon SoulSilver:I" "$CORRECTED"
 
 #Spellcheck "Pokémon Ruby"
 
@@ -60,9 +69,67 @@ sed -i "s:PokémonEmerald:Pokémon Emerald:I" "$CORRECTED"
 sed -i "s:PokémonEmrald:Pokémon Emerald:I" "$CORRECTED"
 sed -i "s:Pokémon Emrald:Pokémon Emerald:I" "$CORRECTED"
 
+#Spellcheck "Pokémon FireRed"
+
+sed -i "s:Pokémon FireRed:Pokémon FireRed:I" "$CORRECTED"
+sed -i "s:PokémonFireRed:Pokémon FireRed:I" "$CORRECTED"
+sed -i "s:Pokémon Fire Red:Pokémon FireRed:I" "$CORRECTED"
+sed -i "s:PokémonFire Red:Pokémon FireRed:I" "$CORRECTED"
+sed -i "s:Pokémon fired:Pokémon FireRed:I" "$CORRECTED"
+
+#Spellcheck "Pokémon LeafGreen"
+
+sed -i "s:Pokémon LeafGreen:Pokémon LeafGreen:I" "$CORRECTED"
+sed -i "s:Pokémon Leaf Green:Pokémon LeafGreen:I" "$CORRECTED"
+sed -i "s:PokémonLeaf Green:Pokémon LeafGreen:I" "$CORRECTED"
+sed -i "s:Pokémonleafgreen:Pokémon LeafGreen:I" "$CORRECTED"
+
+#Spellcheck "Pokémon Gold"
+
+sed -i "s:PokémonGold:Pokémon Gold:I" "$CORRECTED"
+sed -i "s:Pokémon Gold:Pokémon Gold:I" "$CORRECTED"
+
+#Spellcheck "Pokémon Silver"
+
+sed -i "s:PokémonSilver:Pokémon Silver:I" "$CORRECTED"
+sed -i "s:Pokémon Silver:Pokémon Silver:I" "$CORRECTED"
+
+#Spellcheck "Pokémon Crystal"
+
+sed -i "s:PokémonCrystal:Pokémon Crystal:I" "$CORRECTED"
+sed -i "s:Pokémon Crystal:Pokémon Crystal:I" "$CORRECTED"
+
+#Spellcheck "Pokémon Red"
+
+sed -i "s:PokémonRed:Pokémon Red:I" "$CORRECTED"
+sed -i "s:Pokémon Red:Pokémon Red:I" "$CORRECTED"
+
+#Spellcheck "Pokémon Blue"
+
+sed -i "s:PokémonBlue:Pokémon Blue:I" "$CORRECTED"
+sed -i "s:Pokémon Blue:Pokémon Blue:I" "$CORRECTED"
+sed -i "s:PokémonBleu:Pokémon Blue:I" "$CORRECTED"
+sed -i "s:Pokémon Bleu:Pokémon Blue:I" "$CORRECTED"
+
+#Spellcheck "Pokémon Yellow"
+
+sed -i "s:Pokémon Yellow:Pokémon Yellow:I" "$CORRECTED"
+sed -i "s:PokémonYellow:Pokémon Yellow:I" "$CORRECTED"
+
+#Spellcheck "UnovaRPG"
+
+sed -i "s:unova rpg:UnovaRPG:I" "$CORRECTED"
+sed -i "s:UnovaRPG:UnovaRPG:I" "$CORRECTED"
+
+#Spellcheck "DelugeRPG"
+
+sed -i "s:delugerpg:DelugeRPG:I" "$CORRECTED"
+sed -i "s:deluge rpg:DelugeRPG:I" "$CORRECTED"
+
 #Spellcheck "Abomasnow"
 
 sed -i "s:Aboamsnow:Abomasnow:I" "$CORRECTED"
+sed -i "s:Abomasnow:Abomasnow:I" "$CORRECTED"
 
 #Spellcheck "Abra"
 
