259a3ce9e4e3f4b25f3c6ac8bd4f40fe26668e53
I am not me <impeccabletiming@gmail.com>
2014-12-04 22:06:42 -0500

    Adding numerous fixes in Language script

--- Language/Language.sh
+++ Language/Language.sh
@@ -10,6 +10,22 @@ sed -i "s:Spanish:Spanish:I" "$CORRECTED"
 
 sed -i "s:English:English:I" "$CORRECTED"
 
+#Spellcheck "French"
+
+sed -i "s:French:French:I" "$CORRECTED"
+
+#Spellcheck "Greek"
+
+sed -i "s:Greek:Greek:I" "$CORRECTED"
+
+#Spellcheck "Chinese"
+
+sed -i "s:Chinese:Chinese:I" "$CORRECTED"
+
+#Spellcheck "Hebrew"
+
+sed -i "s:Hebrew:Hebrew:I" "$CORRECTED"
+
 #Spellcheck "verb"
 
 sed -i "s:\bvreb:verb:I" "$CORRECTED"
